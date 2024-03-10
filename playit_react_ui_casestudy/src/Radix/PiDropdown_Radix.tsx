89e937b33ce91f4f86e83e1c2012f69938692070
Willem <willem@playitsafe.eu>
2024-03-10 13:54:21 +0100

    animate the dropdown arrow

--- playit_react_ui_casestudy/src/Radix/PiDropdown_Radix.tsx
+++ playit_react_ui_casestudy/src/Radix/PiDropdown_Radix.tsx
@@ -2,20 +2,26 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {ChevronDownIcon} from "@radix-ui/react-icons";
 import React from "react";
-
+import classNames from "classnames";
 export default function PiDropdown_Radix(props: PiSelectProps) {
     const {selectedIndex, values, onChange} = {...props};
-
-    const options = values.map(value => {
-        return <DropdownMenu.Item className={"hover:bg-pibluehover h-36 outline-none cursor-pointer flex align-middle"} key={value}>
+    const [isOpen, setIsOpen] = React.useState(false);
+    
+    const options = values.map((value, index) => {
+        return <DropdownMenu.Item className={"hover:bg-pibluehover h-36 outline-none cursor-pointer flex align-middle"} key={value} onClick={() => onChange(index)}>
             <div className={"px-2 font-poppins my-auto"}>{value}</div>
         </DropdownMenu.Item>
     });
 
-    return <><DropdownMenu.Root>
+    return <><DropdownMenu.Root onOpenChange={setIsOpen}>
         <DropdownMenu.Trigger className={"flex font-poppins box-border w-full px-2 border-2 rounded-full border-solid border-piblue h-36 focus:outline-none"}>
-            <span className={"mt-auto mb-auto"}>{values[selectedIndex]}</span>
-            <div className={"ml-auto mr-2 mt-auto mb-auto"}>
+            <span className={"mt-auto mb-auto select-none"}>{values[selectedIndex]}</span>
+            <div className={classNames({
+                "ml-auto mr-2 my-auto": true,
+                "transition-transform ease-in-out duration-100": true,
+                "transform rotate-180": isOpen,
+                "transform rotate-0": !isOpen,
+            })}>
                 <ChevronDownIcon width={20} height={20}/>
             </div>
         </DropdownMenu.Trigger>
